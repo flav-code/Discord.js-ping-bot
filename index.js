31327a2ee548ee2a5eebb2d5f3c31d15dd2a9a4e
flav <48295189+flav28@users.noreply.github.com>
2022-11-06 13:47:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({intents:32768});
 const config = require("./config.json");
 
 const ping = require('ping');
