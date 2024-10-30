042435357d58d9784a20d1d803e16240d3b28546
flav <48295189+flav-code@users.noreply.github.com>
2024-10-30 19:35:10 +0100

    Fix typo in `services.length` check

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on("ready", () => {
             const obj = JSON.parse(data);
 
             const services = obj.ips;
-            if (services.lenght === 0) return console.log("No ip in config");
+            if (services.length === 0) return console.log("No ip in config");
 
             const guild = client.guilds.cache.get(obj.guild_id) || await client.guilds.fetch(obj.guild_id);
 
